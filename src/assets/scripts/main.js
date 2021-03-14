efa0e0d4f104590751e530a0b38142716a57d099
Alejandro Rodriguez Vignote <vignote@uoc.edu>
2021-03-14 14:48:55 +0100

    Solución Inicial

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,29 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+
+document.querySelector('.header__hamburguer-menu').addEventListener('click', () => {
+  console.log('Entra');
+  const menu = document.querySelector(".main-nav");
+  if (menu.style.display=="block"){
+    menu.style.display="none";
+  }else {
+    menu.style.display="block";
+  } 
+})
+
+
+const nav_items = document.querySelectorAll('.main-nav__link')
+nav_items.forEach (nav_item =>{
+  nav_item.addEventListener('click', () => {
+    console.log('pulsado');
+    if (screen.width<768){
+      const menu = document.querySelector(".main-nav");
+      menu.style.display="none";
+    }
+  
+  });
+})
+
+
